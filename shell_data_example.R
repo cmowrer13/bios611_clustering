74149b8e530a72e6ccd268e27befc3c4a17fa155
cmowrer13 <130422485+cmowrer13@users.noreply.github.com>
2025-10-22 19:52:47 -0400

    Add example shell data and .html visualization

---
+++ shell_data_example.R
@@ -0,0 +1,13 @@
+library(htmlwidgets)
+
+source("generate_shell_data.R")
+
+shell_data <- generate_shell_clusters(n_shells = 4, k_per_shell = 200, max_radius = 5, noise_sd = 0.1)
+
+p <- plot_ly(shell_data, x = ~X1, y = ~X2, z = ~X3, color = ~factor(shell),
+        colors = RColorBrewer::brewer.pal(4, "Set1"),
+        type = "scatter3d", mode = "markers",
+        marker = list(size = 3, opacity = 0.7)) %>%
+  layout(scene = list(aspectmode = "data"))
+
+saveWidget(p, "figures/shell_data_example.html")
